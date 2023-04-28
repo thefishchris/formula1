f7ba61448f84b5628375a71a4170e0a936181df4
thefishchris <chris@knc-us.com>
2023-04-28 19:32:37 -0400

    new file:   .gitignore 	new file:   index.html 	new file:   mock/schedule.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/motogp.svg 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/RaceDay.jsx 	new file:   src/assets/f1.png 	new file:   src/assets/react.svg 	new file:   src/css/riders.css 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import RaceDay from './RaceDay'
+import { Hidden } from '@mui/material';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Card variant="outlined"
+        style={{
+          boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+          backgroundColor: "#f2f2f2",
+        }}>
+        <Box>
+          <h1>NEXT RACE:</h1>
+          <center><RaceDay /></center>
+        </Box>
+      </Card>
+    </div>
+  )
+}
+
+export default App
